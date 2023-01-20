fa661b65065fb882e228643b5bf07915b0e47304
Swagatika-dalai <83580248+Swagatika-dalai@users.noreply.github.com>
2023-01-20 17:38:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,20 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket" "b" {
-  bucket = "swag8645iyygfgukk965fvgafk"
+resource "aws_s3_bucket" "poc4_bktt_s3" {
+  count = var.region == "us-west-1" ? 1 : 0
+  bucket = "hsghgsho135fgshh"
+}
+
+resource "aws_s3_bucket" "poc564_bktt_s3" {
+  count = var.region == "us-east-1" ? 1 : 0
+  bucket = "afvjjoiyr5uua8hh990gv"
+}
+
+resource "aws_s3_bucket" "poc874_bktt_s3" {
+  count = var.region == "us-west-1" ? 1 : 0
+  bucket = "afvjjoi2bsh3ch990gv"
+}
   
 
   tags = {
